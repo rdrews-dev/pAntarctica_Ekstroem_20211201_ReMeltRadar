6080ad6579481f0d1724cc419b665cf2b4f2c995
Jono Hawkins <uceeawk@ucl.ac.uk>
2022-02-02 14:04:16 +0000

    Commit prior to longtable changes

--- Proc/ApRES/Rover/HF/sql/drop_data_table.sql
+++
@@ -1,3 +0,0 @@
--- SQLite
-DROP TABLE IF EXISTS `table_name`;
-DROP TABLE IF EXISTS `data`;
---
+++ Src/ApRES/Rover/HF/catalogue_data.py
@@ -0,0 +1,101 @@
+from os import pathsep
+import sqlite3
+import pathlib
+
+DB_ROOT = "./Doc/ApRES/Rover/HF"
+DB_NAME = "Testing.db"
+
+class ApRESDatabase:
+
+    def __init__(self, path, create=False):
+        
+        # Check path argument is correct
+        if not isinstance(path, pathlib.Path):
+            # case to pathlib.Path
+            path = pathlib.Path(path)
+
+        if not path.exists() and not create:
+            raise FileNotFoundError(f"{path} not found.  If you want to create an object try with create=True parameter.")
+
+        # Assign path to object
+        self.path = path
+        self.db_con = None
+
+        # Try and get connection to database
+        try:
+            self.get_connection(create)
+        except UninitializedDatabaseError:
+            # if not possible then initialise database
+            # if create option has been passed:
+                if create:
+                    self.init_tables()
+        
+    def get_connection(self, create=False):
+        
+        if self.db_con == None:
+            
+            # Check database path exists
+            if self.path == None:
+                raise ValueError(f"No path assigned to locate database file")
+            
+            if not self.path.exists() and not create:
+                raise FileNotFoundError(f"{self.path} not found.  If you want to create an object try with create=True parameter.")
+
+            # Connect to database
+            self.db_con = sqlite3.connect(self.path)
+            cursor = self.db_con.cursor()
+            # Check if database has data table
+            cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='data';")
+            if len(cursor.fetchall()) == 0:
+                raise UninitializedDatabaseError(self.path)
+
+        # Return sqlite3 connection object
+        return self.db_con
+
+    def get_cursor(self):
+        connection = self.get_connection()
+        return connection.cursor()
+
+    def init_tables(self):
+        cursor = self.get_cursor()
+        # Create measurements table
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS `measurements` (
+                measurement_id INTEGER PRIMARY KEY,
+                filename TEXT NOT NULL,
+                path TEXT UNIQUE NOT NULL,
+                name TEXT,
+                time TEXT UNQIUE ASC NOT NULL,
+                location TEXT,
+                comments TEXT,
+                lattiude REAL,
+                longitude REAL,
+                elevation REAL
+            );
+        ''')
+
+        cursor.execute('''
+            CREATE TABLE 
+        ''')
+
+        cursor.execute{'''
+            CREATE TABLE IF NOT EXISTS `data` (
+                data_id INTEGER PRIMARY KEY,
+                measurement_id INTEGER NOT NULL,
+                filename TEXT NOT NULL,
+                path TEXT NOT NULL,
+                time TEXT ASC NOT NULL,
+                processing_steps TEXT
+            )
+        '''}
+
+    
+
+
+
+class UninitializedDatabaseError(Exception):
+    pass
+
+if __name__ == "__main__":
+    # Create database
+    db = ApRESDatabase(pathlib.Path(DB_ROOT) / DB_NAME, create=True)
