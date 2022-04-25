5f131750e144edaa9ae99d58efbc7e5a9655ceef
rdrews-dev <51914390+rdrews-dev@users.noreply.github.com>
2022-04-25 10:43:53 +0200

    Introduction
    Modified introduction to mirror AWI project report. Change Jono's figure names as my latex compile couldn't handle the "\_" in the file names.

--- Src/PulseEKKO/RDTesting.py
+++ Src/PulseEKKO/RDTesting.py
@@ -10,11 +10,14 @@ sys.path.append('./impdar/')
 
 from lib import load
 
-CoreDirectory='/esd/esd01/data/rdrews/data_large/Data_Antarctica/2021_RemeltRadar/Raw/Raw_PE/'
-LinesetDirectory='20220104_GL_FL_50_MHz_along_567/20220104_GL_FL_50_MHz_along_5/Lineset/'
+#CoreDirectory='/esd/esd01/data/rdrews/data_large/Data_Antarctica/2021_RemeltRadar/Raw/Raw_PE/'
+CoreDirectory='/Users/rdrews/Desktop/tmp/data/'
+LinesetDirectory='20220104_GL_FL_50_MHz_along_5/Lineset/'
+LinesetDirectory='20220104_GL_FL_50_MHz_along_6/Lineset'
+LinesetDirectory='20220104_GL_FL_50_MHz_along_7/Lineset'
 print(f"{CoreDirectory}{LinesetDirectory}/line3.dt1")
 dat = load.load("pe",[f"{CoreDirectory}{LinesetDirectory}/line3.dt1"])[0]
-
+dir(dat)
 
 
 
